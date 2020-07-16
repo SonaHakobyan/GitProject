3ea99cdab7dd8929b340668f0317487acdd90947
Sona <hakobyansona.98@gmail.com>
2020-07-16 15:02:15 +0400

    Revert "Revert "Revert "p"""
    This reverts commit 770acf26afff79d663faeb5e3e79e9bfa1910ff2.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             var str = "Hello World!";
-            new Class1().LocalPrint(str);
+            new Class1().LocalPrint_Chackout(str);
         }
     }
 }
