e832ef4c9a9a5e19ee37ecd4bc4c1fa7934e6564
Sona <hakobyansona.98@gmail.com>
2020-07-16 17:38:11 +0400

    local change 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var str = "local change";
+            var str = "local change_2";
             new Class1().LocalPrint(str);
             new Class1().LocalPrint("Local Change");
         }
