793447f125b83d44075e85000876d6b0c0620bfd
Sona <hakobyansona.98@gmail.com>
2020-07-16 17:45:53 +0400

    local change no conflict

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,8 +4,9 @@
     {
         static void Main(string[] args)
         {
-            var str = "local change_2";
+            var str = "local change";
             new Class1().LocalPrint(str);
+            new Class1().LocalPrint("Local Change");
         }
     }
 }
