5690da439440606ad981d939ea97c47e920265d1
Sona <hakobyansona.98@gmail.com>
2020-07-16 16:59:35 +0400

    commit 1

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine(str);
             Console.WriteLine("cherry-pick");
-            Console.WriteLine("one commit push");
+            Console.WriteLine("commit_1");
             return str;
         }
     }
