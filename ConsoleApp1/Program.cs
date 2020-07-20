fcb41735fdc341df744f23803bb8c001a647268b
sona <hakobyansona.98@gmail.com>
2020-07-20 18:10:25 +0400

    hello

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var str = "local";
+            var str = "local-Hello";
             new Class1().LocalPrint(str);
         }
     }
