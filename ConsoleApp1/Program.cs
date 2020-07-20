2060849768f9812ba115d30cf577e04a6e00000b
sona <hakobyansona.98@gmail.com>
2020-07-20 17:18:25 +0400

    removed jjj

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var str = "strjjjj";
+            var str = "str";
             new Class1().LocalPrint(str);
             new Class1().LocalPrint("Local Change");
         }
